71a56c9bc3502da1bf96299fca6f14a4c03ac73d
wang tianlu <tianluwang@epiclouds.net>
2016-07-13 15:47:52 +0800

    fix avg time = 0.0

--- src/others/bc_urls.py
+++ src/others/bc_urls.py
@@ -1,1 +1,25 @@
 __author__ = 'TianluWang'
+
+import re
+
+requests = {}
+with open('/Users/koala/PycharmProjects/ERP_backend_monitor/data/access.log') as f:
+    for line in f.readlines():
+        line_list = line.split(' ')
+        url = line_list[5]
+        if '/bc/' not in url:
+            continue
+        match = re.match(r'(.*)\d', url)
+        if match is not None:
+            url_reverse = url[::-1]
+            pos = url_reverse.index('/')
+            url = url[:-(pos+1)]
+        if url in requests.keys():
+            requests[url] += 1
+        else:
+            requests[url] = 1
+
+for item in requests:
+    print item
+    print requests[item]
+    print '**********'
