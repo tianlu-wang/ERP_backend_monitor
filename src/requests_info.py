2e32e466ae408a21f4dc63705da38a65328837fa
wang tianlu <tianluwang@epiclouds.net>
2016-07-18 11:39:58 +0800

    fix time to time_cost in fields

--- src/requests_info.py
+++ src/requests_info.py
@@ -33,7 +33,7 @@ def request_info(files):
                     value['country'] = ip_location(value['ip'])
 
                     value['status'] = line_list[7]
-                    value['time'] = line_list[-3]
+                    value['time_cost'] = line_list[-3]
                     value['Android'] = 'Android' in line
                     value['iPhone'] = 'iPhone' in line
                     value['url'] = line_list[5]
--- src/static_files.py
+++ src/static_files.py
@@ -24,7 +24,7 @@ def static_files(files):
                     value['ip'] = line_list[0]
                     value['country'] = ip_location(value['ip'])
                     value['status'] = line_list[7]
-                    value['time'] = line_list[-3]
+                    value['time_cost'] = line_list[-3]
                     value['Android'] = 'Android' in line
                     value['iPhone'] = 'iPhone' in line
                     value['url'] = line_list[5]
